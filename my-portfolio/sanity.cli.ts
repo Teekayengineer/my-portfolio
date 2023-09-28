38895c39c657f910f8bae55aa90c4c7ef8ac3ada
Teekayengineer <tanakakamuti@gmail.com>
2023-09-28 08:34:34 +0200

    adding sanity studio

---
+++ my-portfolio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4an77mno',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+    projectId: "4an77mno",
+    dataset: "production"
+})
